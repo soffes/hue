270c667a6a0ef08eb64196230b97df9fddc15c85
Sam Soffes <sam@soff.es>
2013-03-20 23:56:03 -0400

    Modify light attributes

--- lib/hue/light.rb
+++ lib/hue/light.rb
@@ -1,15 +1,15 @@
 module Hue
   class Light
+    HUE_RANGE = 0..65535
+    SATURATION_RANGE = 0..255
+    BRIGHTNESS_RANGE = 0..255
+    COLOR_TEMPERATURE_RANGE = 153..500
+
     # Unique identification number.
     attr_reader :id
 
     # A unique, editable name given to the light.
     attr_accessor :name
-
-    # Brightness of the light. This is a scale from the minimum
-    # brightness the light is capable of, 0, to the maximum capable
-    # brightness, 255. Note a brightness of 0 is not off.
-    attr_accessor :brightness
 
     # Hue of the light. This is a wrapping value between 0 and 65535.
     # Both 0 and 65535 are red, 25500 is green and 46920 is blue.
@@ -19,12 +19,20 @@ module Hue
     # and 0 is the least saturated (white).
     attr_accessor :saturation
 
-    # The x and y coordinates of a color in CIE color space.
-    # The first entry is the x coordinate and the second entry is the
-    # y coordinate. Both x and y are between 0 and 1.
+    # Brightness of the light. This is a scale from the minimum
+    # brightness the light is capable of, 0, to the maximum capable
+    # brightness, 255. Note a brightness of 0 is not off.
+    attr_accessor :brightness
+
+    # The x coordinate of a color in CIE color space. Between 0 and 1.
     #
     # @see http://developers.meethue.com/coreconcepts.html#color_gets_more_complicated
-    attr_accessor :xy
+    attr_reader :x
+
+    # The y coordinate of a color in CIE color space. Between 0 and 1.
+    #
+    # @see http://developers.meethue.com/coreconcepts.html#color_gets_more_complicated
+    attr_reader :y
 
     # The Mired Color temperature of the light. 2012 connected lights
     # are capable of 153 (6500K) to 500 (2000K).
@@ -77,6 +85,10 @@ module Hue
       refresh
     end
 
+    def [](index)
+      lights[index]
+    end
+
     def name=(new_name)
       unless (1..32).include?(new_name.length)
         raise InvalidValueForParameter, 'name must be between 1 and 32 characters.'
@@ -101,12 +113,16 @@ module Hue
       @state['on']
     end
 
-    def on=(new_state)
-      self.set_state(new_state)
+    %w{on hue saturation brightness color_temperature}.each do |key|
+      define_method "#{key}=".to_sym do |value|
+        set_state({key.to_sym => value})
+        instance_variable_set("@#{key}".to_sym, value)
+      end
     end
 
-    def hsb
-      @state.select { |k, v| %w{sat bri hue}.include?(k) }
+    def set_xy(x, y)
+      set_state({:xy => [x, y]})
+      @x, @y = x, y
     end
 
     # Indicates if a light can be reached by the bridge. Currently
@@ -116,42 +132,58 @@ module Hue
       @state['reachable']
     end
 
-    def set_state(attributes, transition = 400)
-      # TODO: Translate keys
+    # @param transition The duration of the transition from the light’s current
+    #   state to the new state. This is given as a multiple of 100ms and defaults
+    #   to 4 (400ms). For example, setting transistiontime:10 will make the
+    #   transition last 1 second.
+    def set_state(attributes, transition = nil)
+      map = {
+        :brightness => :bri,
+        :saturation => :sat,
+        :color_temperature => :ct,
+      }
+
+      body = {}
+      attributes.each do |key, value|
+        new_key = map[key]
+        key = new_key if new_key
+        body[key] = value
+      end
 
       # Add transition
-      attributes.merge!({:transitiontime => transition})
+      body.merge!({:transitiontime => transition}) if transition
 
       uri = URI.parse("#{base_url}/state")
       http = Net::HTTP.new(uri.hostname)
-      response = http.request_put(uri.path, MultiJson.dump(attributes))
+      response = http.request_put(uri.path, MultiJson.dump(body))
       MultiJson.load(response.body)
     end
 
-  private
-
-    def base_url
-      bridge_ip = @client.base_station['internalipaddress']
-      "http://#{bridge_ip}/api/#{@client.username}/lights/#{self.id}"
-    end
-
+    # Refresh the state of the lamp
     def refresh
       json = MultiJson.load(Net::HTTP.get(URI.parse(base_url)))
 
       @state = json['state']
-      @brightness = @state['Bri']
+      @brightness = @state['bri']
       @hue = @state['hue']
-      @saturation = @state['Sat']
-      @xy = @state['xy']
+      @saturation = @state['sat']
+      @x, @y = @state['xy']
       @color_temperature = @state['ct']
-      @alert = @state['alert']
-      @effect = @state['effect']
+      @alert = @state['alert'].to_sym
+      @effect = @state['effect'].to_sym
       @color_mode = @state['colormode']
       @type = json['type']
       @name = json['name']
       @model = json['modelid']
       @software_version = json['swversion']
       @point_symbol = json['pointsymbol']
+    end
+
+  private
+
+    def base_url
+      bridge_ip = @client.base_station['internalipaddress']
+      "http://#{bridge_ip}/api/#{@client.username}/lights/#{id}"
     end
   end
 end
